d26279b0749c4712d41ec6abf097c1bad1d2093a
Jamie Pinheiro <pinheirojamie@google.com>
2020-07-17 19:25:45 +0000

    Make OSS friendly

--- tests/demuxer/information_extraction_test.cc
+++ tests/demuxer/information_extraction_test.cc
@@ -1,5 +1,16 @@
 // Copyright 2020 Google LLC
-// Author: pinheirojamie
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "gtest/gtest.h"
 #include "absl/strings/str_format.h"
@@ -64,8 +75,7 @@ std::string GetPhotoBytesFromXmp(const std::string &xmp_metadata) {
 namespace libmphoto {
 
 TEST(InformationExtraction, CanParseValidMotionPhoto) {
-  std::string photo_bytes =
-      GetBytesFromFile("sample_data/motionphoto.jpg");
+  std::string photo_bytes = GetBytesFromFile("sample_data/motionphoto.jpg");
 
   Demuxer demuxer;
   ImageInfo image_info;
@@ -82,8 +92,7 @@ TEST(InformationExtraction, CanParseValidMotionPhoto) {
 }
 
 TEST(InformationExtraction, CanParseValidMicrovideo) {
-  std::string photo_bytes =
-      GetBytesFromFile("sample_data/microvideo.jpg");
+  std::string photo_bytes = GetBytesFromFile("sample_data/microvideo.jpg");
 
   Demuxer demuxer;
   ImageInfo image_info;
