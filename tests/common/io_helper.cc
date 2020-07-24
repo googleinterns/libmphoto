799d975f5855e0085d74955d89ffde0599343ec6
Jamie Pinheiro <pinheirojamie@google.com>
2020-07-24 15:58:33 +0000

    Update existing tests + add remuxer tests

--- tests/common/io_helper.cc
+++ tests/common/io_helper.cc
@@ -15,20 +15,16 @@
 #include "tests/common/io_helper.h"
 
 #include <fstream>
-#include <iostream>
-#include <vector>
+#include <sstream>
 
 namespace libmphoto {
 
 // Returns the bytes of a file read.
 std::string GetBytesFromFile(const std::string &file_name) {
   std::ifstream file(file_name, std::ifstream::in | std::ifstream::binary);
-  file.seekg(0, std::ios::end);
-  std::streampos length = file.tellg();
-  file.seekg(0, std::ios::beg);
-  std::vector<uint8_t> data(length);
-  file.read(reinterpret_cast<char *>(data.data()), length);
-  return std::string(data.begin(), data.end());
+  std::ostringstream ostream;
+  ostream << file.rdbuf();
+  return std::string(ostream.str());
 }
 
 }  // namespace libmphoto
---
+++ tests/remuxer/jpeg_remuxing_test.cc
@@ -0,0 +1,87 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "libmphoto/demuxer/demuxer.h"
+#include "libmphoto/remuxer/remuxer.h"
+#include "tests/common/io_helper.h"
+
+namespace libmphoto {
+
+TEST(JpegRemuxing, CanRemuxWithNoExistingXmp) {
+  std::string still_bytes = GetBytesFromFile("sample_data/jpeg/no_xmp.jpeg");
+  std::string video_bytes = GetBytesFromFile("sample_data/mp4/video.mp4");
+
+  Remuxer remuxer;
+  EXPECT_TRUE(remuxer.SetStill(still_bytes).ok());
+  EXPECT_TRUE(remuxer.SetVideo(video_bytes).ok());
+
+  std::string motion_photo;
+  EXPECT_TRUE(remuxer.Finalize(&motion_photo).ok());
+
+  std::string expected_remuxed_bytes =
+      GetBytesFromFile("sample_data/remuxed/jpeg/no_xmp.jpeg");
+
+  EXPECT_TRUE(motion_photo == expected_remuxed_bytes);
+
+  Demuxer demuxer;
+  EXPECT_TRUE(demuxer.Init(motion_photo).ok());
+
+  ImageInfo image_info;
+  EXPECT_TRUE(demuxer.GetInfo(&image_info).ok());
+
+  EXPECT_EQ(image_info.motion_photo, 1);
+  EXPECT_EQ(image_info.motion_photo_version, 1);
+  EXPECT_EQ(image_info.motion_photo_presentation_timestamp_us, 0);
+  EXPECT_EQ(image_info.image_mime_type, MimeType::kImageJpeg);
+  EXPECT_EQ(image_info.video_mime_type, MimeType::kVideoMp4);
+  EXPECT_EQ(image_info.video_length, video_bytes.length());
+}
+
+TEST(JpegRemuxing, CanRemuxWithOnlyMotionPhotoXmp) {
+  std::string still_bytes =
+      GetBytesFromFile("sample_data/jpeg_motion_photo/still.jpeg");
+  std::string video_bytes = GetBytesFromFile("sample_data/mp4/video.mp4");
+
+  Remuxer remuxer;
+  EXPECT_TRUE(remuxer.SetStill(still_bytes).ok());
+  EXPECT_TRUE(remuxer.SetVideo(video_bytes).ok());
+
+  std::string motion_photo;
+  EXPECT_TRUE(remuxer.Finalize(&motion_photo).ok());
+
+  std::string expected_remuxed_bytes =
+      GetBytesFromFile("sample_data/remuxed/jpeg/motion_photo_xmp.jpeg");
+
+  EXPECT_TRUE(motion_photo == expected_remuxed_bytes);
+
+  Demuxer demuxer;
+  EXPECT_TRUE(demuxer.Init(motion_photo).ok());
+
+  ImageInfo image_info;
+  EXPECT_TRUE(demuxer.GetInfo(&image_info).ok());
+
+  EXPECT_EQ(image_info.motion_photo, 1);
+  EXPECT_EQ(image_info.motion_photo_version, 1);
+  EXPECT_EQ(image_info.motion_photo_presentation_timestamp_us, 0);
+  EXPECT_EQ(image_info.image_mime_type, MimeType::kImageJpeg);
+  EXPECT_EQ(image_info.video_mime_type, MimeType::kVideoMp4);
+  EXPECT_EQ(image_info.video_length, video_bytes.length());
+}
+
+}  // namespace libmphoto
